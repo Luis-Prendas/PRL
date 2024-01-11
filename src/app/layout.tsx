3c176d2e0ee439324b894cad5e0c06dfb3e9b05a
Luis Daniel Prendas Chavarria <luisprendas.dev@gmail.com>
2024-01-11 10:34:05 -0600

    ui/ux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
+import { FaGithub } from "react-icons/fa"
+import { FaLinkedin } from "react-icons/fa"
+import { SiGmail } from "react-icons/si"
+import { FaLinkSlash } from "react-icons/fa6"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +21,49 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} text-neutral-700 pt-28 bg-slate-100 flex items-center flex-col gap-8`}>
+        <nav className='bg-white shadow-lg absolute top-0 left-0 w-full flex justify-center p-4'>
+          <div className='w-full max-w-screen-lg flex justify-between items-center'>
+            <h1 className='hover:scale-105 transition md:text-3xl text-2xl font-semibold '>
+              <Link href='https://porfolio-luis-prendas.vercel.app/' target='_blank'>
+                DanielPrendas.Dev
+              </Link>
+            </h1>
+            <ul className='flex gap-8'>
+              <li>
+                <Link href='https://github.com/Luis-Prendas' target='_blank'>
+                  <FaGithub className='text-4xl opacity-30 hover:opacity-100 hover:scale-105 transition' />
+                </Link>
+              </li>
+              <li>
+                <Link href='https://www.linkedin.com/in/luisprendasdev/' target='_blank'>
+                  <FaLinkedin className='text-4xl opacity-30 hover:opacity-100 hover:scale-105 transition' />
+                </Link>
+              </li>
+              <li>
+                <Link href='luisprendas.dev@gmail.com' target='_blank'>
+                  <SiGmail className='text-4xl opacity-30 hover:opacity-100 hover:scale-105 transition' />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className='flex items-center justify-center flex-col gap-4 md:gap-6'>
+          <div className='flex gap-2 px-4 pb-3 pt-4 items-center border shadow-sm bg-sky-200 text-sky-600 rounded-full uppercase'>
+            <FaLinkSlash /> <h1>Acortador de links</h1>
+          </div>
+          <h2 className=' text-3xl md:text-6xl text-center text-neutral-800'>
+            De código abierto y gratis
+          </h2>
+          <div className='text-3xl md:text-6xl bg-gradient-to-r from-emerald-400 to-sky-500 text-white rounded-md px-4 pb-2 pt-3'>
+            Pruébalo ahora!
+          </div>
+        </div>
+        {children}
+        <p className='absolute bottom-8 text-sm md:text-xl text-neutral-500 max-w-xs md:max-w-2xl text-center'>
+          Este servicio de acortado de enlaces está hecho por hobby y tiene la finalidad de simplificar la compartición de URL. No me hago responsable del contenido de los enlaces acortados ni respaldo sus destinos.
+        </p>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@
 import Link from "next/link"
 import { FormEvent, useState } from "react"
 import { GifsTypes } from "../../types/supabase"
+import { FaCopy } from "react-icons/fa"
+import { FaExternalLinkSquareAlt } from "react-icons/fa"
+import copy from 'clipboard-copy'
 
 export default function Home() {
   const [input, setInput] = useState('')
   const [data, setData] = useState<GifsTypes | null>(null)
+  const [copied, setCopied] = useState<boolean>(false)
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
@@ -23,34 +27,38 @@ export default function Home() {
     setData(res)
   }
 
-  return (
-    <main className='w-full pt-40 flex justify-center items-center flex-col gap-8'>
-      <form onSubmit={handleSubmit}>
-        <input className="text-black" type="text" placeholder='https://www.example.com' value={input} onChange={(e) => setInput(e.target.value)} />
-      </form>
-      <section className="flex flex-col justify-center items-center gap-6">
-        {!data ? null : (
-          <>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <span className="text-amber-800 bg-amber-200 py-2 px-4 rounded-full">
-                Original link
-              </span>
-              <Link href={data.original_link} target="_blank">
-                {data.original_link}
-              </Link>
-            </div>
+  const handleCopyToClipboard  = (textToCopy: string) => {
+    copy(textToCopy)
+    setCopied(true)
 
-            <div className="flex flex-col justify-center items-center gap-2">
-              <span className="text-amber-800 bg-amber-200 py-2 px-4 rounded-full">
-                Shortened link
-              </span>
-              <Link href={data.link_shortened} target="_blank">
-                {data.link_shortened}
+    setTimeout(() => {
+      setCopied(false)
+    }, 3000)
+  }
+
+  return (
+    <main className='w-full h-full flex items-center flex-col gap-8'>
+      <form onSubmit={handleSubmit} className="flex gap-4 justify-center items-center">
+        <input className="text-black py-2 px-4 rounded-lg w-72 md:w-96 shadow-lg hover:scale-105 transition border border-neutral-300" type="text" placeholder='https://www.example.com' value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit" className="w-32 py-2 px-4 bg-gradient-to-tr from-fuchsia-600 to-rose-600 text-white rounded-lg shadow-lg hover:scale-105 transition">Acortar</button>
+      </form>
+      <section className="flex flex-col justify-center items-center gap-4 text-center">
+
+        {!data ? null : (
+          <div className="flex justify-center items-center gap-4">
+            <input value={data.link_shortened} className="text-black py-2 px-4 rounded-lg w-72 md:w-96 shadow-lg hover:scale-105 transition border border-neutral-300" readOnly />
+            <div className="flex gap-4 text-2xl">
+              <button onClick={() => handleCopyToClipboard(data.link_shortened)} className="w-12 py-2 px-4 pr-10 bg-gradient-to-tr from-fuchsia-600 to-rose-600 text-white rounded-lg shadow-lg hover:scale-105 transition">
+                <FaCopy />
+              </button>
+              <Link href={data.link_shortened} target="_blank" className="w-12 py-2 px-4 pr-10 bg-gradient-to-tr from-fuchsia-600 to-rose-600 text-white rounded-lg shadow-lg hover:scale-105 transition">
+                <FaExternalLinkSquareAlt />
               </Link>
             </div>
-          </>
+          </div>
         )}
       </section>
+      {copied && <p>Copiado al portapapeles!</p>}
     </main>
   )
 }
